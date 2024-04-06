43c2e2015d5470686e72a84eb810cfb5cd3f37f3
Felipe <pipecalgaro@gmail.com>
2024-04-06 12:11:18 -0300

    feat: add PollsSection and QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { CreatePollForm } from './components/CreatePollForm'
+import { PollsSection } from './components/PollsSection'
+import { useState } from 'react'
+
 import './global.css'
 
+const queryClient = new QueryClient()
+
+export type Poll = {
+  id: string
+  title: string
+}
+
 function App() {
+  const [polls, setPolls] = useState<Poll[]>([])
+
   return (
-    <main className="flex justify-center items-center bg-zinc-950 min-h-screen">
-      <CreatePollForm />
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="flex justify-center items-center bg-zinc-950 min-h-screen">
+        <CreatePollForm setPolls={setPolls} />
+        <PollsSection polls={polls} />
+      </main>
+    </QueryClientProvider>
   )
 }
 
---
+++ src/components/PollsSection.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import { Poll } from '../App'
+
+interface PollsSectionProps {
+  polls: Poll[]
+}
+
+export function PollsSection({ polls }: PollsSectionProps) {
+  return (
+    <section className='flex flex-col gap-8 justify-start items-center'>
+      {polls.map((poll) => (
+        <Link to={`/polls/${poll.id}`} className='text-black bg-slate-400 py-3'>
+          <h1>{poll.title}</h1>
+        </Link>
+      ))}
+    </section>
+  )
+}
